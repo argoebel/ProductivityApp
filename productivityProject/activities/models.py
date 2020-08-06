983d876a3bfa55cf48d180ab547e625b592a7ab8
Austin Goebel <argoebel@ualberta.ca>
2020-08-06 13:23:01 -0600

    updated activity API to datetime and duration fields

--- productivityProject/activities/models.py
+++ productivityProject/activities/models.py
@@ -3,5 +3,7 @@ from django.db import models
 # Create your models here.
 class Activity(models.Model):
     title = models.CharField(max_length=100)
-    time = models.CharField(max_length=100)
-    duration = models.CharField(max_length=100)
+    time = models.DateTimeField()
+    duration = models.DurationField()
+    description = models.CharField(max_length=500)
+    private = models.BooleanField(default=False)
--- productivityProject/activities/serializers.py
+++ productivityProject/activities/serializers.py
@@ -3,8 +3,10 @@ from .models import Activity
 
 class ActivitySerializer(serializers.ModelSerializer):
     title = serializers.CharField()
-    time = serializers.CharField()
-    duration = serializers.CharField()
+    time = serializers.DateTimeField()
+    duration = serializers.DurationField()
+    description = serializers.CharField()
+    private = serializers.BooleanField()
     
     class Meta:
         model = Activity
